bd5506c89d5a2da453e1ed26727a7fee7629b393
Aishwarya <56058723+aishwarya-rane@users.noreply.github.com>
2020-08-17 01:11:06 -0700

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,6 +1,6 @@
 #libraries
 import nltk
-from nltk.corpus 
+from nltk.corpus
 import random
 
 # Load and prepare the dataset
@@ -9,3 +9,7 @@ documents = [(list(movie_reviews.words(fileid)), category)
               for fileid in movie_reviews.fileids(category)]
 
 random.shuffle(documents)
+
+# Define the feature extractor
+all_words = nltk.FreqDist(w.lower() for word in movie_reviews.words())
+word_features = list(all_words)[:2000]
